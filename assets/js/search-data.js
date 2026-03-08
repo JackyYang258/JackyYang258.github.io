cfe860a5ea4b1a507bffa2946ffadbdcfc92f235
JackyYang258 <yang1213853@gmail.com>
2026-03-08 03:56:01 +0000

    Deploying to gh-pages from @ JackyYang258/JackyYang258.github.io@43cbb2c092da1d1fd95f9eea7d1cf462f201fc50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,16 +409,16 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-latent-wasserstein-adversarial-imitation-learning",
-          title: 'Latent Wasserstein Adversarial Imitation Learning',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
+            },},{id: "projects-latent-wasserstein-adversarial-imitation-learning",
+          title: 'Latent Wasserstein Adversarial Imitation Learning',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/lwail/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
